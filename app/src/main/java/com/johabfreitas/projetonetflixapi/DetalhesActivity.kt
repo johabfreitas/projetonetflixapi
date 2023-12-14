3d2970748e07b4d0cf094af02c8e127a839c2908
Johab Freitas <freitasjohab@gmail.com>
2023-12-14 09:41:24 -0300

    Configurando o getParcelable

--- app/src/main/java/com/johabfreitas/projetonetflixapi/DetalhesActivity.kt
+++ app/src/main/java/com/johabfreitas/projetonetflixapi/DetalhesActivity.kt
@@ -3,7 +3,7 @@ package com.johabfreitas.projetonetflixapi
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.annotation.RequiresApi
+//import androidx.annotation.RequiresApi
 import com.johabfreitas.projetonetflixapi.api.RetrofitService
 import com.johabfreitas.projetonetflixapi.databinding.ActivityDetalhesBinding
 import com.johabfreitas.projetonetflixapi.model.Filme
@@ -15,29 +15,34 @@ class DetalhesActivity : AppCompatActivity() {
         ActivityDetalhesBinding.inflate(layoutInflater)
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+   //@RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         val bundle = intent.extras
+        //val filme = bundle?.getParcelable("filme", Filme::class.java)
 
-        val filme = bundle?.getParcelable("filme", Filme::class.java)
+        if(bundle != null) {
+            val filme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                bundle.getParcelable("filme", Filme::class.java)
+            } else {
+                bundle.getParcelable("filme") as? Filme
+            }
 
-        if(filme != null){
-            binding.textFilmeTitulo.text = filme.title
+            if(filme != null){
+                binding.textFilmeTitulo.text = filme.title
 
-            val nomeFilme = filme.backdrop_path
-            val tamanhoFilme = "w780"
-            val urlBase = RetrofitService.BASE_URL_IMAGE
-            val urlFilme = urlBase + tamanhoFilme + nomeFilme
+                val nomeFilme = filme.backdrop_path
+                val tamanhoFilme = "w780"
+                val urlBase = RetrofitService.BASE_URL_IMAGE
+                val urlFilme = urlBase + tamanhoFilme + nomeFilme
 
-            Picasso.get()
-                .load(urlFilme)
-                .into(binding.imgPoster)
+                Picasso.get()
+                    .load(urlFilme)
+                    .into(binding.imgPoster)
+            }
         }
-
     }
-
 }
 
